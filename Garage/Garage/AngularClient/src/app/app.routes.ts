f47d446a186a8a9549eab595349e3abe078328b0
Jadran Grba <jadrangrba@gmail.com>
2024-02-19 12:54:02 +0100

    adds bootstrap and routing

--- Garage/Garage/AngularClient/src/app/app.routes.ts
+++ Garage/Garage/AngularClient/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { CarHistoryComponent } from './pages/car-history/car-history.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'homepage',
+        component: HomepageComponent
+    },
+    {
+        path: 'carhistory',
+        component: CarHistoryComponent
+    }
+];
