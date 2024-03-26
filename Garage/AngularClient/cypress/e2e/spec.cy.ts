f31959e381cd9d440f2822a307480617800db1a4
Jadran Grba <jadrangrba@gmail.com>
2024-03-26 09:54:28 +0100

    creates docker support for backend

--- Garage/AngularClient/cypress/e2e/spec.cy.ts
+++ Garage/AngularClient/cypress/e2e/spec.cy.ts
@@ -1,5 +1,5 @@
 describe('Homepage', () => {
-  it('document title should be Garage', () => {
+  it('Document title should be Garage', () => {
     cy.visit('/');
     cy.contains('Cars at Service');
     cy.title().should('equal', 'Garage');
@@ -10,7 +10,7 @@ describe('Homepage', () => {
     cy.contains('Cars at Service');
   });
 
-  it('AddNew button should open new form', () => {
+  it('Click AddNew button should open new form', () => {
     cy.visit('/');
     cy.get('[data-testid="addNewCarAtService"]').click();
     cy.contains('Add a car to service');
